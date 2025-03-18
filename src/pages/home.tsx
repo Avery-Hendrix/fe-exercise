49cf92437f190510435191b7fcf949283493f560
Avery Hendrix <avery.a.hendrix@gmail.com>
2025-03-17 22:28:43 -0500

    added fetches for logout button, dog search, dog match, added favorite toggle, added location fetches but needs additional fixes, added breed sorter, added input fields, sorters, updated ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,42 @@
 'use client'
-import { baseURL } from "@/src/components/constants";
+import { baseURL, Dog } from "@/src/components/constants";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import '../_app/globals.css'
+import { useRouter } from "next/navigation";
+
+interface Match {
+  match: string;
+}
+
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
+
+interface Location {
+  // Define the properties of a Location object here
+}
 
 export default function Home() {
+  const router = useRouter();
   const [dogBreeds, setDogBreeds] = useState<string[]>([]);
+  const [searchResults, setSearchResults] = useState<Dog[]>([]);
+  const [favorites, setFavorites] = useState<string[]>([]);
+  const [page, setPage] = useState(0);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+  const [sortField, setSortField] = useState<'breed' | 'name' | 'age'>('breed');
+  const [selectedBreed, setSelectedBreed] = useState<string | null>(null);
+  const [zipCode, setZipCode] = useState<string>('');
+  const [city, setCity] = useState<string>('');
+  const [state, setState] = useState<string>('');
 
   const dogBreedData = async () => {
     try {
       const response = await fetch(baseURL + '/dogs/breeds', {
         method: "GET",
         headers: {
-          "Content-Type" : "application/json",
+          "Content-Type": "application/json",
         },
         credentials: "include"
       });
@@ -21,125 +45,292 @@ export default function Home() {
         throw new Error(`Response status: ${response.status} - ${response.statusText}`);
       }
       return response.json();
-    }
-    catch (error: any) {
+    } catch (error: any) {
       console.error(error.message);
     }
-  }
+  };
 
   useEffect(() => {
     const fetchDogBreeds = async () => {
-        const breeds = await dogBreedData();
-        setDogBreeds(breeds);
+      const breeds = await dogBreedData();
+      setDogBreeds(breeds);
     };
     fetchDogBreeds();
   }, []);
 
-  console.log(dogBreeds)
+  const logoutButton = () => {
+    fetch(baseURL + '/auth/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: "include"
+    })
+      .then((response) => {
+        if (response.ok) {
+          router.push('/')
+        } else {
+          // Handle errors
+        }
+      })
+  };
+
+  const searchDogs = async (queryParams: Record<string, any>) => {
+    const queryString = new URLSearchParams(queryParams).toString();
+    try {
+      const response = await fetch(`${baseURL}/dogs/search?${queryString}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status} - ${response.statusText}`);
+      }
+      const data = await response.json();
+      const dogIds = data.resultIds;
+      const dogsResponse = await fetch(baseURL + '/dogs', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(dogIds),
+      });
+
+      if (!dogsResponse.ok) {
+        throw new Error(`Response status: ${dogsResponse.status} - ${dogsResponse.statusText}`);
+      }
+      return dogsResponse.json();
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      const results = await searchDogs({
+        breeds: selectedBreed ? [selectedBreed] : [dogBreeds.map((breed) => breed)],
+        size: 10,
+        from: page * 10,
+        sort: `${sortField}:${sortOrder}`,
+        zipCode: zipCode || undefined,
+        city: city || undefined,
+        state: state ? [state] : undefined,
+      });
+      setSearchResults(results);
+    };
+    fetchSearchResults();
+  }, [selectedBreed, page, sortOrder, sortField, zipCode, city, state]);
+
+  const toggleFavorite = (dogId: string) => {
+    setFavorites((prevFavorites) =>
+      prevFavorites.includes(dogId)
+        ? prevFavorites.filter((id) => id !== dogId)
+        : [...prevFavorites, dogId]
+    );
+  };
+
+  const generateMatch = async () => {
+    try {
+      const response = await fetch(baseURL + '/dogs/match', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(favorites),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status} - ${response.statusText}`);
+      }
+      const match: Match = await response.json();
+      console.log(match, 'match');
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
+  const postLocations = async (zipCodes: string[]) => {
+    if (zipCodes.length > 100) {
+      throw new Error("The array of ZIP codes should contain no more than 100 items.");
+    }
+    try {
+      const response = await fetch(baseURL + '/locations', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(zipCodes),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
+  const searchLocations = async (queryParams: {
+    city?: string,
+    states?: string[],
+    geoBoundingBox?: {
+      top?: number,
+      left?: number,
+      bottom?: number,
+      right?: number,
+      bottom_left?: Coordinates,
+      top_left?: Coordinates
+    },
+    size?: number,
+    from?: number
+  }) => {
+    try {
+      const response = await fetch(baseURL + '/locations/search', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(queryParams),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
+  const sortedBreeds = [...dogBreeds].sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return a.localeCompare(b);
+    } else {
+      return b.localeCompare(a);
+    }
+  });
 
   return (
-    <div className="richard">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {dogBreeds.map((breed: string) => (
+    <div className="container mx-auto p-4">
+      <header className="flex justify-between items-center mb-8">
+        <h1 className="text-3xl font-bold">Dog Finder</h1>
+        <button onClick={logoutButton} className="bg-red-500 text-white px-4 py-2 rounded">
+          Logout
+        </button>
+      </header>
+      <main className="flex flex-col gap-8 items-center">
+        <div className="flex gap-4">
+          <select
+            onChange={(e) => setSelectedBreed(e.target.value)}
+            value={selectedBreed || ''}
+            className="border p-2 rounded"
+          >
+            <option value="">All Breeds</option>
+            {sortedBreeds.map((breed, index) => (
+              <option key={index} value={breed}>{breed}</option>
+            ))}
+          </select>
+          <select
+            onChange={(e) => setSortField(e.target.value as 'breed' | 'name' | 'age')}
+            value={sortField}
+            className="border p-2 rounded"
+          >
+            {!selectedBreed && <option value="breed">Breed</option>}
+            {selectedBreed && <><option value="name">Name</option>
+            <option value="age">Age</option></>}
+          </select>
           <button
-            key={breed}
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            onClick={() => setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')}
+            className="bg-blue-500 text-white px-4 py-2 rounded"
           >
-            {breed}
+            Sort {sortOrder === 'asc' ? 'Descending' : 'Ascending'}
           </button>
-        ))}
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        </div>
+        <div className="flex gap-4">
+          <input
+            type="text"
+            placeholder="ZIP Code"
+            value={zipCode}
+            onChange={(e) => setZipCode(e.target.value)}
+            className="border p-2 rounded"
+            onSubmit={() => postLocations([zipCode])}
+          />
+          <input
+            type="text"
+            placeholder="City"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            className="border p-2 rounded"
+            onSubmit={() => searchLocations({ city })}
+          />
+          <input
+            type="text"
+            placeholder="State"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+            className="border p-2 rounded"
+            onSubmit={() => searchLocations({ states: [state] })}
+          />
+        </div>
+        {!selectedBreed && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {sortedBreeds.map((breed, index) => (
+              <button
+                className="border p-4 rounded shadow-lg font-bold"
+                key={index}
+                onClick={() => setSelectedBreed(breed)}>
+                  {breed}
+              </button>
+            ))}
+          </div>
+        )}
+        {searchResults && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {searchResults.map((dog) => (
+              <div key={dog.id} className="border p-4 rounded shadow-lg">
+                <Image unoptimized src={dog.img} alt={dog.name} width={200} height={200} />
+                <h3 className="text-xl font-bold mt-2">{dog.name}</h3>
+                <p className="text-gray-700">Breed: {dog.breed}</p>
+                <p className="text-gray-700">Age: {dog.age}</p>
+                <p className="text-gray-700">Zip Code: {dog.zip_code}</p>
+                <button
+                  onClick={() => toggleFavorite(dog.id)}
+                  className={`mt-2 px-4 py-2 rounded ${favorites.includes(dog.id) ? 'bg-red-500 text-white' : 'bg-green-500 text-white'}`}
+                >
+                  {favorites.includes(dog.id) ? 'Remove from Favorites' : 'Add to Favorites'}
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+        <div className="flex gap-4 mt-4">
+          <button
+            onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
+            disabled={page === 0}
+            className="bg-gray-500 text-white px-4 py-2 rounded disabled:opacity-50"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Previous
+          </button>
+          <button
+            onClick={() => setPage((prev) => prev + 1)}
+            className="bg-gray-500 text-white px-4 py-2 rounded"
           >
-            Read our docs
-          </a>
+            Next
+          </button>
+        <button
+          onClick={generateMatch}
+          className="bg-purple-500 text-white px-4 py-2 rounded mt-4"
+        >
+          Generate Match
+        </button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Global
-        </a>
-      </footer>
     </div>
   );
 }
